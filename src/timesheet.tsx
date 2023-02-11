e8b2954f729651f28bd2e4e37864b3690183b0f6
vinhanguyen <vinh.a.nguyen@gmail.com>
2023-02-10 23:10:20 -0500

    Implement tabs and add css

--- src/timesheet.tsx
+++ src/timesheet.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import { getCurrentJobId } from "./data/idb";
 import Jobs from "./jobs";
 import Tasks from "./tasks";
+import './timesheet.css';
 
 export default function Timesheet() {
   const [currentJobId, setCurrentJobId] = useState<number|null>(null);
+  const [tab, setTab] = useState(0);
 
   useEffect(() => {
     (async () => {
@@ -14,8 +16,14 @@ export default function Timesheet() {
 
   return (
     <>
-      <Tasks currentJobId={currentJobId} />
-      <Jobs currentJobId={currentJobId} onChangeJob={setCurrentJobId} />
+      <nav>
+        <ul>
+          <li onClick={() => setTab(0)} className={tab === 0 ? 'active' : ''}>Timesheet</li>
+          <li onClick={() => setTab(1)} className={tab === 1 ? 'active' : ''}>Jobs</li>
+        </ul>
+      </nav>
+      {tab === 0 && <Tasks currentJobId={currentJobId} />}
+      {tab === 1 && <Jobs currentJobId={currentJobId} onChangeJob={setCurrentJobId} />}
     </>
   );
 }
