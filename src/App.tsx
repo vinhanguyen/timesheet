7a33d9d587791abdc4864776336dc2ce9b005631
vinhanguyen <vinh.a.nguyen@gmail.com>
2023-02-13 10:35:48 -0500

    Comment out logo import from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 // import './App.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
