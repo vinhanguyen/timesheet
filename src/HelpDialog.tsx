2505fd4a362f380bc804641b62a329189e35a32b
vinhanguyen <vinh.a.nguyen@gmail.com>
2023-02-15 06:12:37 -0500

    Update help

--- src/HelpDialog.tsx
+++ src/HelpDialog.tsx
@@ -9,7 +9,7 @@ export default function HelpDialog({open, onClose}: any) {
         <ol>
           <li>Select Jobs from Menu ({<Menu />})</li>
           <li>Click Add ({<Add />}) to add jobs</li>
-          <li>Select current job by clicking on its radio button</li>
+          <li>Select radio button of job to work on</li>
           <li>Select Timesheet from Menu ({<Menu />})</li>
           <li>Click Start ({<PunchClock color="success" />}) to start work</li>
           <li>Click Stop ({<PunchClock color="error" />}) to stop work</li>
